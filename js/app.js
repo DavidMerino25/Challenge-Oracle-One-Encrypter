f47bf8838750ef284d88c38f53c5650e82a27615
DavidMerino25 <deividrocan2508@gmail.com>
2022-09-21 21:22:43 -0500

    Css finalizado y parte lógica iniciada

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-var ouputDIV = document.getElementById("ouput-text");
+var ouputDIV = document.getElementById("container-ouput-text");
+var inputText = document.getElementById('text-input');
 var button = document.getElementById('button-copy');
 var outputimage = document.getElementById('ouput-imagen');
+var textOuput = document.getElementById('text-ouput');
 ouputDIV.style.display = "none";
+
+
 function encrypt() {
     ouputDIV.style.display = "block";
     outputimage.style.display = "none";
+    textOuput.value = inputText.value;
 }
 function decrypt() {
     ouputDIV.style.display = "block";
