d6f7fda8e4cc4d4894682010df4233c6c7f3eb6c
DavidMerino25 <deividrocan2508@gmail.com>
2022-09-21 20:30:13 -0500

    ocultar completado

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var ouputDIV = document.getElementById("ouput-text");
+var button = document.getElementById('button-copy');
+var outputimage = document.getElementById('ouput-imagen');
+ouputDIV.style.display = "none";
+function encrypt() {
+    ouputDIV.style.display = "block";
+    outputimage.style.display = "none";
+}
+function decrypt() {
+    ouputDIV.style.display = "block";
+    outputimage.style.display = "none";
+}
